d299ee279cc47cef668acfddfd631d10eb266bc8
edwardvaneechoud <evaneechoud@gmail.com>
2025-08-19 08:42:48 +0200

    bumping version

--- pl_fuzzy_frame_match/__init__.py
+++ pl_fuzzy_frame_match/__init__.py
@@ -5,7 +5,7 @@ pl-fuzzy-match: Efficient Fuzzy Matching for Polars DataFrames.
 from .matcher import fuzzy_match_dfs, fuzzy_match_dfs_with_context, fuzzy_match_temp_dir
 from .models import FuzzyMapping, FuzzyTypeLiteral
 
-__version__ = "0.2.0"  # Keep in sync with pyproject.toml
+__version__ = "0.3.0"
 
 __all__ = [
     "fuzzy_match_dfs",
